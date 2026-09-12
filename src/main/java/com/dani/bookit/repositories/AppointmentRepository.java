ba71b6a6c8124681d8912b8bf1add60bbe398e72
Daniel Juan <dannijl88web@gmail.com>
2026-09-12 16:48:54 +0200

    feat: add AppointmentRepository with findByClient

--- src/main/java/com/dani/bookit/repositories/AppointmentRepository.java
+++ src/main/java/com/dani/bookit/repositories/AppointmentRepository.java
@@ -1,7 +1,13 @@
 package com.dani.bookit.repositories;
 
 import com.dani.bookit.entities.Appointment;
+import com.dani.bookit.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    List<Appointment> findByClient(User client);
+
 }
