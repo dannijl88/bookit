5f5d0042010df963c637cec63433508ad9ded190
Daniel Juan <dannijl88web@gmail.com>
2026-09-15 13:39:29 +0200

    add LoginRequestDto

---
+++ src/main/java/com/dani/bookit/dto/LoginRequestDto.java
@@ -0,0 +1,20 @@
+package com.dani.bookit.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequestDto {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
